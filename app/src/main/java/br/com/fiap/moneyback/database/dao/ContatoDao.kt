bc60a031a517b6e88e0640ab6fa6760076f4153d
fpaludetto <146347087+fpaludetto@users.noreply.github.com>
2024-05-20 10:33:34 -0300

    20/05

--- app/src/main/java/br/com/fiap/moneyback/database/dao/ContatoDao.kt
+++ app/src/main/java/br/com/fiap/moneyback/database/dao/ContatoDao.kt
@@ -29,6 +29,36 @@ interface ContatoDao {
     @Query("SELECT * FROM tbl_cadmentor ORDER BY nome ASC")
     fun listarContatos(): List<Contato>
 
+
+    //@Query("SELECT * FROM tbl_cadmentor mentor ORDER BY nome ASC")
+    //@Query("SELECT * FROM tbl_cadmentor mentor WHERE  mentor.nome = :pNome  ORDER BY nome ASC")
+    //@Query("SELECT * FROM tbl_cadmentor mentor, tbl_aluno aluno WHERE mentor.area_expertise = aluno.area_interesse and mentor.id = :id  ORDER BY nome ASC")
+    //@Query("SELECT mentor.nome || aluno.nome as nome, mentor.telefone, mentor.area_expertise, mentor.especializacao, mentor.disponibilidade FROM tbl_cadmentor mentor, tbl_aluno aluno WHERE mentor.area_expertise = aluno.area_interesse ORDER BY nome ASC")
+
+
+    @Query("SELECT " +
+            "mentor.id, " +
+            "mentor.nome as nome, " +
+            //"'Mentor:' || mentor.nome || ' - Aluno:' || aluno.nome as nome, " +
+            "mentor.telefone, " +
+            "mentor.formacao," +
+            "mentor.area_expertise, " +
+            "mentor.especializacao, " +
+            "mentor.disponibilidade, " +
+            "mentor.tempo_experiencia, " +
+            "mentor.aula_online " +
+            "FROM " +
+            "tbl_cadmentor mentor, " +
+            "tbl_aluno aluno " +
+            "WHERE " +
+            "mentor.area_expertise = aluno.area_interesse " +
+            //"and mentor.nome like 'D%'" +
+            "and mentor.nome != :pNome "+
+            "ORDER BY nome ASC")
+
+    fun buscarRelMentorAluno(pNome: String): List<Contato>
+    //fun buscarRelMentorAluno(): List<Contato>
+
 }
 
 
@@ -44,10 +74,11 @@ interface ContatoDao {
         @Delete
         fun excluir(tbl_aluno: Tbl_aluno): Int
 
-        @Query("SELECT * FROM tbl_aluno WHERE id = :id")
-        fun buscarAlunoPeloId(id: Long): Tbl_aluno
+        //@Query("SELECT * FROM tbl_aluno WHERE id = :id")
+        //fun buscarAlunoPeloId(id: Int): Tbl_aluno
 
         @Query("SELECT * FROM tbl_aluno ORDER BY nome ASC")
         fun listarAlunos(): List<Tbl_aluno>
 
     }
+
